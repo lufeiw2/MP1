6a5a471ea25ceb10515885c6bed1f16268dd6eab
lufeiw2 <139701015+lufeiw2@users.noreply.github.com>
2024-01-28 23:51:26 -0500

    Update serve.py

--- serve.py
+++ serve.py
@@ -9,8 +9,8 @@ def stress_cpu():
     # Run stress_cpu.py in a separate process
     process = Popen(["python3", "stress_cpu.py"], stdout=PIPE, stderr=PIPE)
     std_out, std_err = process.communicate()
-    output = stdout.decode('utf-8')
-    error = stderr.decode('utf-8')
+    output = std_out.decode('utf-8')
+    error = std_err.decode('utf-8')
     print(output, error)
     return {'message': 'CPU stress initiated'}, 200
 
