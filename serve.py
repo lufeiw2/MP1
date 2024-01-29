c57925cd349c82cafba562ef4ea12b7e2060283f
lufeiw2 <139701015+lufeiw2@users.noreply.github.com>
2024-01-29 01:19:18 -0500

    Update serve.py

--- serve.py
+++ serve.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 def stress_cpu():
     # Run stress_cpu.py in a separate process
+    output = ''
+    error = ''
     process = Popen(["python3", "/MP1/stress_cpu.py"], stdout=PIPE, stderr=PIPE)
-    std_out, std_err = process.communicate()
-    output = std_out.decode('utf-8')
-    error = std_err.decode('utf-8')
-    print(output, error)
+    # std_out, std_err = process.communicate()
+    # output = std_out.decode('utf-8')
+    # error = std_err.decode('utf-8')
+    # print(output, error)
     return {'message': 'CPU stress initiated v1', 'output': output, 'error': error}, 200
 
 @app.route("/", methods=["GET"])
